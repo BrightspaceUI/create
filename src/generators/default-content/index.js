da52fd5651571468946113893c432349e30f5217
Dave Lockhart <dlockhart@users.noreply.github.com>
2020-12-14 11:56:06 -0500

    adding Dependabot

--- src/generators/default-content/index.js
+++ src/generators/default-content/index.js
@@ -46,5 +46,10 @@ export function run(templateData) {
 		`${getDestinationPath(templateData.hyphenatedName)}/LICENSE`
 	);
 
+	copyFile(
+		`${__dirname}/templates/configured/dependabot.yml`,
+		`${getDestinationPath(templateData.hyphenatedName)}/.github/dependabot.yml`
+	);
+
 	copyFilesInDir(`${__dirname}/templates/static`, getDestinationPath(templateData.hyphenatedName));
 }
