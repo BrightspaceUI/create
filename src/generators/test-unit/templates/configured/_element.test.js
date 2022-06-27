383fd7281cc2d629e3da3c0a7e48aacf96d23ea5
Dave Lockhart <dlockhart@users.noreply.github.com>
2022-06-27 14:49:10 -0400

    putting core back

--- src/generators/test-unit/templates/configured/_element.test.js
+++ src/generators/test-unit/templates/configured/_element.test.js
@@ -1,5 +1,6 @@
 import '../<%= hyphenatedName %>.js';
 import { expect, fixture, html } from '@open-wc/testing';
+import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
 
 describe('<%= className %>', () => {
 
@@ -8,15 +9,12 @@ describe('<%= className %>', () => {
 			const el = await fixture(html`<<%= tagName %>></<%= tagName %>>`);
 			await expect(el).to.be.accessible();
 		});
+	});
 
-		describe('constructor', () => {
-			it('should construct', () => {
-				const ctor = customElements.get('<%= tagName %>');
-				expect(ctor).to.not.be.undefined;
-				document.createElement('<%= tagName %>');
-			});
+	describe('constructor', () => {
+		it('should construct', () => {
+			runConstructor('<%= tagName %>');
 		});
-
 	});
 
 });
